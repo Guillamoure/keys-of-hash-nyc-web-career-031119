e050f637cbbd39c132738cc8884b9b8c586376b9
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-07 01:01:38 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,8 +2,13 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     array = []
-      binding.pry
-      
+      if arguments.length > 1
+        argument_string = arguments.join(", ")
+        i = values.index(argument_string)
+        
+        answer = keys[i]
+        
+      end
     return answer
   end
 end
